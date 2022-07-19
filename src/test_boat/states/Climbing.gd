96209d4ed83e441b741ba9ddba37c17e9460a91e
qez008 <morten_bergmann@live.no>
2022-07-19 22:17:57 +0200

    move boat files into vessel folder

--- src/test_boat/states/Climbing.gd
+++
@@ -1,8 +0,0 @@
-extends StateMachine.State
-
-
-var body: Boat
-
-
-func _init(sm).(sm, "idle"):
-    pass
--- src/test_boat/states/Surfing.gd
+++
@@ -1,8 +0,0 @@
-extends StateMachine.State
-
-
-var body: Boat
-
-
-func _init(sm).(sm, "surfing"):
-    pass
